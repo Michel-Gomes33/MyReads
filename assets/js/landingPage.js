ac5ab4dc1cb2f643904a817c78363e5485b5ffff
Michel-Gomes33 <michel.lrgomes@outlook.com>
2025-01-19 20:18:41 -0500

    Added all html, js, and css files with comments

---
+++ assets/js/landingPage.js
@@ -0,0 +1,21 @@
+// Create const for:
+// - title 
+// - author 
+// - genre 
+// - date posted 
+// - platform posted 
+// - star rating 
+// - review 
+// - summary 
+
+// Create storage for website 
+
+// Create object for array with above i 
+
+// Create function to clear input prompts when submitted
+
+// Create error messages
+
+// Set Date completed into date float 
+
+
