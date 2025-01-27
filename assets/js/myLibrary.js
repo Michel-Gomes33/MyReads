c7187ef8f8ae7a494bbb4710377f0f94a5cf6713
Michel-Gomes33 <michel.lrgomes@outlook.com>
2025-01-26 22:17:46 -0500

    updated-all-codes

--- assets/js/myLibrary.js
+++ assets/js/myLibrary.js
@@ -1,69 +1,33 @@
-let myEntries = localStorage.getItem(formDatas)
-console.log(myEntries)
-//     let mainEl = document.querySelector('main')
-// // TODO: Create a function that builds an element and appends it to the DOM
-// function createCard(el, textInfo, appendEl) {
-//     let createdElement = document.createElement(el)
-//     createdElement.textContent = textInfo 
-//     appendEl.appendChild(createdElement)
-//     return createdElement
-// }
-// // TODO: Create a function that handles the case where there are no blog posts to display
-// function handleNoData() {
-//     createCard('h2', "No Blog posts yet...", mainEl)
-// }
-// // TODO: Create a function called `renderBlogList` that renders the list of blog posts if they exist. If not, call the no posts function.
+let bookReview= readLocalStorage()
+    console.log(bookReview)
+    let mainEl = document.querySelector('main')
 
-// function renderBlogList(){
-//     if (blogPosts.length === 0) {
-//         handleNoData()
-//     } else {
-//         for( let i = 0 ; i < blogPosts.length; i++ ) {
-//             let parent = createCard("article", null, mainEl);
-//             createCard("h2", blogPosts[i].title, parent)
-//             createCard("blockquote", blogPosts[i].content, parent)
-//             createCard("p", blogPosts[i].username, parent)
-//             parent.classList.add("card")
-//         }
-//     }
-// }
-// // TODO: Call the `renderBlogList` function
-// renderBlogList()
+function createCard(el, textInfo, appendEl) {
+    let createdElement = document.createElement(el)
+    createdElement.textContent = textInfo 
+    appendEl.appendChild(createdElement)
+    return createdElement
+}
 
+function handleNoData() {
+    createCard('h2', "No Blog posts yet...", mainEl)
+}
 
-
-
-
-
-
-
-
-
-
-
-
-// // DOM Loader
-
-
-
-
-// document.addEventListener("DOMContentLoaded", () => {
-   
-
-// function addCard(formDatas) {
-//     const container = document.getElementById('inputContainer');
-//     const card = document.getElementById('div');
-//     card.className = "card";
-//     card.innerHTML = `
-//             <img src="${data.thumbnail}" alt="${data.title}" />
-//             <div class="card-title">${data.title}</div>
-//             <div class="card-rating">${"⭐".repeat(data.rating)}</div>
-//             <div class="card-summary-short">${data.summary.split(" ")[0]}...</div>
-//             <div class="card-summary">${data.summary}</div>
-//         `;
-
-//         // Append to the container
-//         container.appendChild(card);
-//     }
-// }
-// })
+function renderBlogList(){
+    if (bookReview.length === 0) {
+        handleNoData();
+    } else {
+        for (let i = 0; i < bookReview.length; i++) {
+            let parent = createCard("article", null, mainEl);
+            createCard("h2", bookReview[i].title, parent);  
+            createCard("blockquote", bookReview[i].genre, parent);  
+            createCard("h3", bookReview[i].starRating, parent); 
+            createCard("p", bookReview[i].date, parent);  
+            createCard("p", bookReview[i].review, parent);  
+            let summaryElement = createCard("p", bookReview[i].summary, parent); 
+            summaryElement.classList.add("summary");  
+            parent.classList.add("card");
+        }
+    }
+}
+console.log('hi')
